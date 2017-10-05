de12014890108864c51623896cd3251d214217c9
William S. Moses <gh@wsmoses.com>
2017-10-05 00:30:24 -0400

    Update deb builder && dockerfile

--- deb.sh
+++ deb.sh
@@ -26,7 +26,7 @@ echo "" >> debian/control
 echo "Package: $NAME" >> debian/control
 echo "Architecture: amd64" >> debian/control
 echo "Provides: llvm, clang, libllvm-tp, clang, clang-tp" >> debian/control
-echo "Depends: ${shlibs:Depends}, ${misc:Depends}" >> debian/control
+echo "Depends: ${shlibs:Depends}, ${misc:Depends}, libcilkrts5, libc6, libc6-dev, binutils, libgcc1, libgcc-5-dev" >> debian/control
 echo "Description: Tapir LLVM + Clang distribution" >> debian/control
 echo "" >> debian/control
 #Dev package
